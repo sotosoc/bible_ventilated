74293c8dfa5cebdbb3ac08cc8a0f11655e74250a
Don Cassidy <sotosoc@protonmail.com>
2022-05-01 17:27:46 -0600

    aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,21 @@
+export BIBLE=$HOME/repos/web_bible_ventilated
+alias :b='cd $BIBLE/books'
+
+alias :1ps='cd $BIBLE/books/1psalms'
+alias :1th='cd $BIBLE/books/1thessalonians'
+alias :2ps='cd $BIBLE/books/2psalms'
+alias :2th='cd $BIBLE/books/2thessalonians'
+alias :3ps='cd $BIBLE/books/3psalms'
+alias :4ps='cd $BIBLE/books/4psalms'
+alias :5ps='cd $BIBLE/books/5psalms'
+alias :jdg='cd $BIBLE/books/judges'
+alias :jud='cd $BIBLE/books/jude'
+
+alias :1psalms='cd $BIBLE/books/1psalms'
+alias :2psalms='cd $BIBLE/books/2psalms'
+alias :3psalms='cd $BIBLE/books/3psalms'
+alias :4psalms='cd $BIBLE/books/4psalms'
+alias :5psalms='cd $BIBLE/books/5psalms'
+alias :jude='cd $BIBLE/books/jude'
+alias :judges='cd $BIBLE/books/judges'
+
